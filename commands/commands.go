abed91d90869b6754115ced413ac3ebc3463ab59
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-16 21:15:31 -0500

    moving some code around

---
+++ commands/commands.go
@@ -0,0 +1,130 @@
+package commands
+
+import (
+    "net/http"
+    "encoding/json"
+    "os"
+    "fmt"
+)
+
+type LocationAreas struct {
+    Count    int        `json:"count"`
+    Next     *string    `json:"next"`
+    Previous *string    `json:"previous"`
+    Results  []struct {
+        Name string     `json:"name"`
+        URL  string     `json:"url"`
+    } `json:"results"`
+}
+
+type CliCommand struct {
+    Name string
+    Description string
+    Callback func(*CliCommandConfig) error
+}
+
+// TODO if no file outside of this one needs to access CliCommandConfig fields, stop exporting them
+type CliCommandConfig struct {
+    NextLocationsPageUrl string
+    PrevLocationsPageUrl string
+}
+
+var Commands map[string]CliCommand
+
+func init() {
+    Commands = map[string]CliCommand {
+        "exit": {
+            Name: "exit",
+            Description: "Exit the Pokedex",
+            Callback: commandExit,
+        },
+        "help": {
+            Name: "help",
+            Description: "Displays commands and their usage information",
+            Callback: commandHelp,
+        },
+        "map": {
+            Name: "map",
+            Description: "Display the next 20 locations in the Pokemon world",
+            Callback: commandMap,
+        },
+        "mapb": {
+            Name: "mapb",
+            Description: "Display the previous 20 locations in the Pokemon world",
+            Callback: commandMapB,
+        },
+    }
+}
+
+func InitCommandConfig() CliCommandConfig {
+    return CliCommandConfig {
+        NextLocationsPageUrl: "https://pokeapi.co/api/v2/location-area",
+        PrevLocationsPageUrl: "",
+    }
+}
+
+func commandExit(config *CliCommandConfig) error {
+    fmt.Println("Closing the Pokedex... Goodbye!")
+    os.Exit(0)
+    // We'll never actually reach this return
+    return nil
+}
+
+func commandHelp(config *CliCommandConfig) error {
+    for _, command := range Commands {
+        fmt.Printf("%s: %s\n", command.Name, command.Description)
+    }
+
+    return nil
+}
+
+func getLocationAreas(config *CliCommandConfig, pageUrl string) error {
+    res, err := http.Get(pageUrl)
+    if err != nil { return err }
+    defer res.Body.Close()
+
+    if res.StatusCode < 200 || res.StatusCode > 299 {
+        return fmt.Errorf("request failure - response code %s", res.Status)
+    }
+
+    var data LocationAreas
+    if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+        return err
+    }
+
+    if data.Next != nil {
+        config.NextLocationsPageUrl = *data.Next;
+    } else {
+        config.NextLocationsPageUrl = "";
+    }
+
+    if data.Previous != nil {
+        config.PrevLocationsPageUrl = *data.Previous;
+    } else {
+        config.PrevLocationsPageUrl = "";
+    }
+
+    for _, location := range data.Results {
+        fmt.Println(location.Name)
+    }
+
+    return nil
+}
+
+func commandMap(config *CliCommandConfig) error {
+    if config.NextLocationsPageUrl == "" {
+        fmt.Println("You are already on the last page")
+        return nil
+    }
+
+    return getLocationAreas(config, config.NextLocationsPageUrl)
+}
+
+func commandMapB(config *CliCommandConfig) error {
+    if config.PrevLocationsPageUrl == "" {
+        fmt.Println("You are already on the first page")
+        return nil
+    }
+
+    return getLocationAreas(config, config.PrevLocationsPageUrl)
+}
--- main.go
+++ main.go
@@ -5,147 +5,30 @@ import (
     "bufio"
     "os"
     "fmt"
-    "net/http"
-    "encoding/json"
+
+    "github.com/vedaRadev/pokedexcli-boot.dev/commands"
 )
 
-type LocationAreas struct {
-    Count    int        `json:"count"`
-    Next     *string    `json:"next"`
-    Previous *string    `json:"previous"`
-    Results  []struct {
-        Name string     `json:"name"`
-        URL  string     `json:"url"`
-    } `json:"results"`
-}
-
-type cliCommand struct {
-    name string
-    description string
-    callback func(*cliCommandConfig) error
-}
-
-type cliCommandConfig struct {
-    nextLocationsPageUrl string
-    prevLocationsPageUrl string
-}
-
-var COMMANDS map[string]cliCommand 
-
-func commandExit(config *cliCommandConfig) error {
-    fmt.Println("Closing the Pokedex... Goodbye!")
-    os.Exit(0)
-    // We'll never actually reach this return
-    return nil
-}
-
-func commandHelp(config *cliCommandConfig) error {
-    for _, command := range COMMANDS {
-        fmt.Printf("%s: %s\n", command.name, command.description)
-    }
-
-    return nil
-}
-
-func getLocationAreas(config *cliCommandConfig, pageUrl string) error {
-    res, err := http.Get(pageUrl)
-    if err != nil { return err }
-    defer res.Body.Close()
-
-    if res.StatusCode < 200 || res.StatusCode > 299 {
-        return fmt.Errorf("request failure - response code %s", res.Status)
-    }
-
-    var data LocationAreas
-    if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
-        return err
-    }
-
-    if data.Next != nil {
-        config.nextLocationsPageUrl = *data.Next;
-    } else {
-        config.nextLocationsPageUrl = "";
-    }
-
-    if data.Previous != nil {
-        config.prevLocationsPageUrl = *data.Previous;
-    } else {
-        config.prevLocationsPageUrl = "";
-    }
-
-    for _, location := range data.Results {
-        fmt.Println(location.Name)
-    }
-
-    return nil
-}
-
-func commandMap(config *cliCommandConfig) error {
-    if config.nextLocationsPageUrl == "" {
-        fmt.Println("You are already on the last page")
-        return nil
-    }
-
-    return getLocationAreas(config, config.nextLocationsPageUrl)
-}
-
-func commandMapB(config *cliCommandConfig) error {
-    if config.prevLocationsPageUrl == "" {
-        fmt.Println("You are already on the first page")
-        return nil
-    }
-
-    return getLocationAreas(config, config.prevLocationsPageUrl)
-}
-
 func main() {
-    // TODO Is there a way to initialize this outside of main without causing an initialization
-    // cycle due to the help command callback referencing the global variable?
-    COMMANDS = map[string]cliCommand {
-        "exit": {
-            name: "exit",
-            description: "Exit the Pokedex",
-            callback: commandExit,
-        },
-        "help": {
-            name: "help",
-            description: "Displays commands and their usage information",
-            callback: commandHelp,
-        },
-        "map": {
-            name: "map",
-            description: "Display the next 20 locations in the Pokemon world",
-            callback: commandMap,
-        },
-        "mapb": {
-            name: "mapb",
-            description: "Display the previous 20 locations in the Pokemon world",
-            callback: commandMapB,
-        },
-    }
-
     fmt.Println("Welcome to the Pokedex!")
     scanner := bufio.NewScanner(os.Stdin)
-    commandConfig := cliCommandConfig {
-        nextLocationsPageUrl: "https://pokeapi.co/api/v2/location-area",
-        prevLocationsPageUrl: "",
-    }
-    var lastCommand cliCommand
+    commandConfig := commands.InitCommandConfig()
+    var lastCommand commands.CliCommand
     for {
         fmt.Print("Pokedex > ")
         scanner.Scan()
         input := cleanInput(scanner.Text())
 
         if len(input) == 0 {
-            if lastCommand.callback != nil {
-                lastCommand.callback(&commandConfig)
+            if lastCommand.Callback != nil {
+                lastCommand.Callback(&commandConfig)
             }
 
             continue
         }
 
-        if command, exists := COMMANDS[input[0]]; exists {
-            command.callback(&commandConfig)
+        if command, exists := commands.Commands[input[0]]; exists {
+            command.Callback(&commandConfig)
             lastCommand = command
         } else {
             fmt.Printf("unrecognized command: %s\n", input[0])
